1b84c9c6c6d3568672104712a4d356c79beadc44
Ridhima Garg <ridhimagarg82.2012@gmail.com>
2025-02-18 12:46:08 +0530

    Solution to Print a Square Pattern

--- Print a Square Pattern@@67877e1818d53eb724f435be/code.c
+++ Print a Square Pattern@@67877e1818d53eb724f435be/code.c
@@ -7,7 +7,7 @@ int main() {
     
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            printf("*");
+            printf("* ");
         }
         printf("\n");
     }
